c2ed718ffd4abcf4bdf6b70e43ff32f06ad1a976
Marian Horoiu <horoiu@yahoo.com>
2018-03-20 09:52:46 +0200

    Implement dice roll and update HTML for roundScore of each player

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('JS is loaded');
+// console.log('JS is loaded');
+
+
+let scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+   
+    let dice = Math.floor(Math.random() * 6) + 1;
+    
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'img/dice-' + dice + '.png';
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+    }
+
+});
